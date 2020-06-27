4d3c8bd135fd6352984c2fae20cb7f0a90ee40a5
OlafurjonHI <ojv1@hi.is>
2020-06-27 21:05:31 +0000

    Firebase auth for email/pass

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,31 @@ const config = {
     appId: "1:682083868299:web:fd5e321ed351cc5caa7e8e"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}}`);
+    const snapshot = await userRef.get();
+    if(!snapshot.exists){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error Creating user', error.message)
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
@@ -21,5 +45,7 @@ const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
 
 export const signInWithGoogle  = () => auth.signInWithPopup(provider);
+
+
 
 export default firebase;
